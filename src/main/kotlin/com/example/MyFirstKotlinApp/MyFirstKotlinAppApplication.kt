50b3c4cb8782d083e9ce7b1ab5617441b57f9db2
LunaTW <68732727+LunaTW@users.noreply.github.com>
2021-01-18 16:30:17 +0800

    Feat-3: Rock-Paper-Scissors

--- src/main/kotlin/com/example/MyFirstKotlinApp/MyFirstKotlinAppApplication.kt
+++ src/main/kotlin/com/example/MyFirstKotlinApp/MyFirstKotlinAppApplication.kt
@@ -1,30 +1,27 @@
 package com.example.MyFirstKotlinApp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class MyFirstKotlinAppApplication
 
 fun main(args: Array<String>) {
-//	runApplication<MyFirstKotlinAppApplication>(*args)
-	// Normal statement
-	var x = 3;
-	x = x*10;
-	print("x is $x.\n");
+	val SongOne = Song("The Mesopotamians","Luna")
+	val SoneTwo = Song("Going Underground","The Jam")
 
-	// Loop statement
-	while (x >20){
-		x = x-1;
-		println("x is $x. ")
-	}
-	for (i in 1..10){
-		x=x+1;
-		println("X now is $x. ");
-	}
-
-	// if - else 若只有一行，可简写
-	println(if (x>3) "X is grater than 3" else "X is smaller than 3")
-
-
+	SongOne.play()
+	SoneTwo.play()
+	SongOne.stop()
 }
+
+class Song(val title: String, val artist: String){
+	fun play(){
+		println("Playing the song $title by $artist")
+	}
+
+	fun stop(){
+		println("Stopped playing $title")
+	}
+}
+
+
